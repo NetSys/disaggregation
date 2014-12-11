7896d55c6ffe52df0b550e67572be5dbf401261f
Peter Gao <petergao@berkeley.edu>
2014-12-11 15:30:12 -0800

    fb scripts

--- fb-trace-analysis/sync.sh
+++ fb-trace-analysis/sync.sh
@@ -2,7 +2,7 @@
 while true; do
     file=$(inotifywait -r /home/peter/disaggregation/fb-trace-analysis/*.py --format %w -q | sed -e "s,/home/peter/disaggregation/fb-trace-analysis/,,g" | tr '\n' ' ')
     echo $file
-    ssh petergao@c33 "rm /root/disaggregation/fb-trace-analysis/$file"
+    ssh petergao@c33 "rm /home/eecs/petergao/disaggregation/fb-trace-analysis/$file"
     #scp -r /home/peter/disaggregation/fb-trace-analysis/*.py petergao@c33:/home/eecs/petergao/fb-trace-analysis
-    rsync -rv --update --include='*/' --include='*.py' --exclude='*' /home/peter/disaggregation/fb-trace-analysis/* root@c127:/root/disaggregation/fb-trace-analysis
+    rsync -rv --update --include='*/' --include='*.py' --exclude='*' /home/peter/disaggregation/fb-trace-analysis/* petergao@c33:/home/eecs/petergao/disaggregation/fb-trace-analysis
 done
