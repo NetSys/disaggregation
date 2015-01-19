c14f09cb9ed5ceccea0dc8962006dd3bde58a789
peter <petergao@berkeley.edu>
2015-01-19 21:18:05 +0000

    rm generated files

--- rmem/.rmem.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/root/disaggregation/rmem/rmem.ko := ld -r -m elf_x86_64 -T /usr/src/linux-headers-3.13.0-32-generic/scripts/module-common.lds --build-id  -o /root/disaggregation/rmem/rmem.ko /root/disaggregation/rmem/rmem.o /root/disaggregation/rmem/rmem.mod.o
