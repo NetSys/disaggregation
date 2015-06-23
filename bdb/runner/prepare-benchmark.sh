e20dc83061340ab4d145feaff7ddfabd823e6596
Peter Gao <petergao@berkeley.edu>
2015-06-23 15:25:52 -0700

    removed bdb

--- bdb/runner/prepare-benchmark.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-cd "`dirname $0`"
-PYTHONPATH="./deps/:$PYTHONPATH" python ./prepare_benchmark.py $@
--- bdb/runner/run-query.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-cd "`dirname $0`"
-PYTHONPATH="./deps/:$PYTHONPATH" python ./run_query.py $@
