35555d9de07152ec9990cfd47236f1a77b942add
petergao <petergao@berkeley.edu>
2016-02-22 22:34:14 +0000

    40g smooth cdf

---
+++ rmem/fcts/rename.sh
@@ -0,0 +1,18 @@
+
+
+mv $1/fcts_dc_pfabric_bdb.txt $1/fcts_tm_pfabric_bdb.txt
+mv $1/fcts_rs_pfabric_bdb.txt $1/fcts_tmrs_pfabric_bdb.txt
+mv $1/fcts_dc_pfabric_graphlab.txt $1/fcts_tm_pfabric_graphlab.txt
+mv $1/fcts_rs_pfabric_graphlab.txt $1/fcts_tmrs_pfabric_graphlab.txt
+mv $1/fcts_dc_pfabric_memcached.txt $1/fcts_tm_pfabric_memcached.txt
+mv $1/fcts_rs_pfabric_memcached.txt $1/fcts_tmrs_pfabric_memcached.txt
+mv $1/fcts_dc_pfabric_terasort_hadoop.txt $1/fcts_tm_pfabric_terasort.txt
+mv $1/fcts_rs_pfabric_terasort_hadoop.txt $1/fcts_tmrs_pfabric_terasort.txt
+mv $1/fcts_dc_pfabric_terasort_spark.txt $1/fcts_tm_pfabric_terasort-spark.txt
+mv $1/fcts_rs_pfabric_terasort_spark.txt $1/fcts_tmrs_pfabric_terasort-spark.txt
+mv $1/fcts_dc_pfabric_timely.txt $1/fcts_tm_pfabric_timely.txt
+mv $1/fcts_rs_pfabric_timely.txt $1/fcts_tmrs_pfabric_timely.txt
+mv $1/fcts_dc_pfabric_wordcount_hadoop.txt  $1/fcts_tm_pfabric_wordcount-hadoop.txt
+mv $1/fcts_rs_pfabric_wordcount_hadoop.txt $1/fcts_tmrs_pfabric_wordcount-hadoop.txt
+mv $1/fcts_dc_pfabric_wordcount_spark.txt $1/fcts_tm_pfabric_wordcount.txt
+mv $1/fcts_rs_pfabric_wordcount_spark.txt $1/fcts_tmrs_pfabric_wordcount.txt
